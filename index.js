1376274a27f7db0d12273a9c1dbec89590139329
한경수 <601107w@gmail.com>
2020-01-18 03:50:12 +0900

    webpackエラーの修正

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-function sum(v1, v2) {
-    return v1 + v2;
-}
-var n = 1;
-var s = 2;
-var result = sum(n, s);
-console.log(result);
+"use strict";
+exports.__esModule = true;
+var deps_ts_1 = require("./deps.ts");
+deps_ts_1["default"](1, 2);
--- index.ts
+++ index.ts
@@ -1,9 +1,2 @@
-function sum(v1: number, v2: number):number {
-	return v1 + v2;
-}
-
-const n = 1;
-const s = 2;
-
-const result = sum(n, s);
-console.log(result);
+import sum from './deps.ts';
+sum(1,2);
