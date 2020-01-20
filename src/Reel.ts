989b5848bde0331c0324e36caec6d787352fdbdf
한경수 <601107w@gmail.com>
2020-01-20 23:14:14 +0900

    メソッド説明追加

--- src/Reel.ts
+++ src/Reel.ts
@@ -17,12 +17,28 @@ export default class Reel extends PIXI.Container {
 
 	// 複数の型を指定した場合、その変数のメソッドを実行する際は型がいずれかであることが明確になっていなければ、
 	// トランスパイル時にエラーとなります。複数の型指定は、パイプで接続します。
-	// ex)
+	// ex) 複数の型指定と型の明確化の例
 	// public multipleTypes(value: string|number): void {
-	// if(typeof value === 'string') {
+	// 	if(typeof value === 'string') {
 	// 		value.replace('foo','bar');
 	// 	}
 	// }
+	// 型の明確化は、「as」を用いて単一の型にキャストすることでも解決されますが、こちらはキャストした型であることの確実性の
+	// 担保が実装者に委ねられる
+	// ex)型キャストの例
+	// public multipleTypes(value: string | number): void {
+	//	(value as string).replace('foo', 'bar');
+	// }
+	// メソッドの引数には、この他にもオプションやデフォルト値などが利用できます。
+	// ex)引数のオプションとデフォルト
+	// public argsVariants(defaultArg: number = 0, optionArg?: number): void {
+	// 	console.log(defaultArg + (optionArg || 0));
+	// }
+	//
+	// ex) 上記のソースを元に生まれるメソッドシグニチャ
+	// argsVariants(): void;
+	// argsVariants(defaultArg: number): void;
+	// argsVariants(defaultArg: number, optionArg:number): void;
 	public update(): void {
 	}
 
