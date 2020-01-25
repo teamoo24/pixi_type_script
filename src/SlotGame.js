507c69471c98a9a4cf7ed6fc68965c5ca2c3031e
한경수 <601107w@gmail.com>
2020-01-25 15:50:01 +0900

    ゲーム起動処理

--- src/SlotGame.js
+++ src/SlotGame.js
@@ -1,10 +1,40 @@
 "use strict";
 exports.__esModule = true;
+// こちらのimport文は"npm install"でインストールされたpixi.jsの全てのAPIを読み込む
+var PIXI = require("pixi.js");
+var UI_1 = require("./UI");
 var SlotGame = /** @class */ (function () {
     function SlotGame() {
+        var _this = this;
+        this.onReady = function () { };
+        if (!document.body) {
+            throw new Error("window is not ready");
+        }
+        this.app = new PIXI.Application({
+            width: SlotGame.width,
+            height: SlotGame.height,
+            backgroundColot: 0x1099bb
+        });
+        document.body.appendChild(this.app.view);
+        for (var i = 0; i < SlotGame.resources.length; i++) {
+            var resource = SlotGame.resources[i];
+            this.app.loader.add(resource);
+        }
+        this.app.loader.load(function () {
+            _this.ui = new UI_1["default"]();
+            _this.onReady();
+        });
     }
     SlotGame.prototype.start = function () {
     };
+    SlotGame.width = 800;
+    SlotGame.height = 640;
+    SlotGame.resources = [
+        '/assets/animalface_kangaroo.png',
+        '/assets/animalface_kirin.png',
+        '/assets/animalface_tanuki.png',
+        '/assets/animalface_usagi.png',
+    ];
     return SlotGame;
 }());
 exports["default"] = SlotGame;
--- src/SlotGame.ts
+++ src/SlotGame.ts
@@ -1,5 +1,41 @@
+// こちらのimport文は"npm install"でインストールされたpixi.jsの全てのAPIを読み込む
+import * as PIXI from 'pixi.js';
+import UI from './UI'
+
 export default class SlotGame {
+	public static readonly width: number = 800;
+	public static readonly height: number = 640;
+	public static readonly resources: string[] = [
+		'/assets/animalface_kangaroo.png',
+		'/assets/animalface_kirin.png',
+		'/assets/animalface_tanuki.png',
+		'/assets/animalface_usagi.png',
+	];
+
+	private app!: PIXI.Application;
+	private ui!:UI;
+	private onReady: () => void = () =>{};
+
 	constructor() {
+		if ( !document.body ) {
+			throw new Error ("window is not ready");
+		}
+		this.app = new PIXI.Application({
+			width : SlotGame.width,
+			height : SlotGame.height,
+			backgroundColot : 0x1099bb,
+		});
+		document.body.appendChild(this.app.view);
+
+		for (let i = 0; i < SlotGame.resources.length; i++) {
+			const resource = SlotGame.resources[i];
+			this.app.loader.add(resource);
+		}
+
+		this.app.loader.load(() => {
+			this.ui = new UI();
+			this.onReady();
+		});
 	}
 
 	public start(): void {
